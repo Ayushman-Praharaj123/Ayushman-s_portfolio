407883ba4cc3e2ba984e6e033c22efd391c2e248
Ayushman Praharaj <ayushmanpraharaj1@gmail.com>
2025-04-13 22:43:56 +0530

    done

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Ayushman's_portfolio/",
+  base: "/Ayushman-s_portfolio/",
   plugins: [
     tailwindcss(),
     react()],
